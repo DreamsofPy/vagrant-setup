e231446e26f97782f1df287930182785ed6d75d2
yeshwanthyk <yeshwanth.yk@gmail.com>
2013-08-01 10:01:59 -0400

    Add updated shell script and Makefile

---
+++ Makefile
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+install:
+	@echo "Create virtualenvwrapper"
+	@echo "export WORKON_HOME=~/virtualenvs" >> ~/.bashrc
+	@echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+	@echo "export PIP_VIRTUALENV_BASE=~/virtualenvs" >> ~/.bashrc
+	@echo '[[ -s "/home/andre/.rvm/scripts/rvm" ]] && source "/home/andre/.rvm/scripts/rvm"' >> ~/.bashrc
+	@sudo rvm install 1.9.3
+	@sudo rvm use --default 1.9.3
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #The DEBIAN_FRONTEND=noninteractive setting will prevent dialogs that ask you to enter settings while installing and/or updating packages and will use the default instead
-export DEBIAN_FRONTEND=noninteractivei
+export DEBIAN_FRONTEND=noninteractive
 
 #Install Python, pip and virtualenv
 apt-get update > /dev/null
@@ -13,10 +13,6 @@ sudo pip install virtualenv
 #Install Virtualenvwrapper
 sudo pip install virtualenvwrapper
 mkdir ~/virtualenvs
-#echo "export WORKON_HOME=~/virtualenvs" >> ~/.bashrc
-#echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
-#echo "export PIP_VIRTUALENV_BASE=~/virtualenvs" >> ~/.bashrc
-#source ~/.bashrc
 
 #Install cURL and mysql
 apt-get -y install curl
@@ -24,11 +20,8 @@ apt-get -y install mysql-client mysql-server libmysql-ruby libmysqlclient-dev
 
 # Install RVM
 bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
-#echo '[[ -s "/home/andre/.rvm/scripts/rvm" ]] && source "/home/andre/.rvm/scripts/rvm"' >> ~/.bashrc
 
 source ~/.bashrc
-#sudo rvm install 1.9.3
-#sudo rvm use --default 1.9.3
 
 #Install Mongo
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
